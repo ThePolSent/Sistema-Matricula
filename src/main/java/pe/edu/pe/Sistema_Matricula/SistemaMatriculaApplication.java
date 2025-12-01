602889e5b38352dee498ffff7b88ac7b467ebd77
ThePolSent <polsent2006@hotmail.com>
2025-12-01 17:13:51 -0500

    Empezando el infierno

---
+++ src/main/java/pe/edu/pe/Sistema_Matricula/SistemaMatriculaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.Sistema_Matricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMatriculaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/pe/Sistema_Matricula/SistemaMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.Sistema_Matricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
